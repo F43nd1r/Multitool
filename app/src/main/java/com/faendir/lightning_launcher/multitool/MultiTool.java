1f8522586a132d6c4089b96af3b07cc18cb1bbdf
F43nd1r <lukas.morawietz@gmail.com>
2017-12-19 23:28:19 +0100

    v1.12 allow badge to be hidden

--- app/src/main/java/com/faendir/lightning_launcher/multitool/MultiTool.java
+++ app/src/main/java/com/faendir/lightning_launcher/multitool/MultiTool.java
@@ -6,18 +6,19 @@ import android.content.Context;
 import org.acra.ACRA;
 import org.acra.annotation.AcraCore;
 import org.acra.annotation.AcraHttpSender;
+import org.acra.data.StringFormat;
 import org.acra.sender.HttpSender;
 
 /**
  * Created by Lukas on 13.12.2015.
  * Main Application class
  */
-@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraCore(buildConfigClass = BuildConfig.class,
+        reportFormat = StringFormat.JSON)
 @AcraHttpSender(uri = "https://faendir.com/acra/report",
         httpMethod = HttpSender.Method.POST,
-        reportType = HttpSender.Type.JSON,
-        basicAuthLogin = "591c604b42110637a8e132c0",
-        basicAuthPassword = "CTgrnKHPk1hjDNX4")
+        basicAuthLogin = "tM7oBAo83wcAmaCK",
+        basicAuthPassword = "56Rb0aGfj697yTMG")
 public class MultiTool extends Application {
     public static final boolean DEBUG = BuildConfig.DEBUG;
     public static final String LOG_TAG = "[MULTITOOL]";
--- app/src/main/res/raw/badge_resume.js
+++ app/src/main/res/raw/badge_resume.js
@@ -3,9 +3,14 @@ eval(loadRawResource("com.faendir.lightning_launcher.multitool","library"));
 bindClass("android.os.Handler");
 
 var item = getEvent().getItem();
+var showZero = bindPrefs(["badgeShowZero"]).badgeShowZero == "true";
 
 var listener = function(count){
-    item.setLabel(count + "");
+    if (!showZero && count == 0) {
+        item.setLabel("");
+    } else {
+        item.setLabel(count + "");
+    }
 }
 item.my.badgeListener = getObjectFactory().constructBadgeListener(new Handler(), getActiveScreen().getContext(), item.getTag("package"), listener);
 item.my.badgeListener.onChange(false);
