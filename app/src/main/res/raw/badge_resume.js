91f5c2cbe657f07168014e37953eade14ed5f4aa
F43nd1r <lukas.morawietz@gmail.com>
2018-03-14 16:23:21 +0100

    hide badge item instead of only label

--- app/src/main/res/raw/badge_resume.js
+++ app/src/main/res/raw/badge_resume.js
@@ -1,15 +1,14 @@
 eval(loadRawResource("com.faendir.lightning_launcher.multitool","library"));
 
-bindClass("android.os.Handler");
-
 var item = getEvent().getItem();
 var showZero = bindPrefs(["badgeShowZero"]).badgeShowZero == "true";
 
 var listener = function(count){
     if (!showZero && count == 0) {
-        item.setLabel("");
+        item.setVisibility(false);
     } else {
         item.setLabel(count + "");
+        item.setVisibility(true);
     }
 }
 item.my.badgeListener = getObjectFactory().constructBadgeListener(new Handler(), getActiveScreen().getContext(), item.getTag("package"), listener);
